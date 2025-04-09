b60136cce207a2ed68e32e175a18f7b8e09c4cbf
Carlos Herbert <hrbrtvieira@gmail.com>
2025-04-08 23:39:34 -0300

    continuando estilização

--- src/components/get-user/getUserGithub.tsx
+++ src/components/get-user/getUserGithub.tsx
@@ -7,6 +7,7 @@ import  Camada  from "../../assets/Camada.png";
 import  Elipse1  from "../../assets/Ellipse_1.png";
 import  Elipse2  from "../../assets/Ellipse_2.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export const GetUserGithub = () : ReactElement => {
     const [nameuser, setNameUser] = useState<string>('')
@@ -14,7 +15,7 @@ export const GetUserGithub = () : ReactElement => {
     const [error, setError] = useState<string>('')
     const [loading, setLoading] = useState<boolean>(true)
     useEffect(() => {
-       const fetchdata = async () => {
+      const fetchdata = async () => {
 
           setLoading(true)
 
@@ -25,7 +26,7 @@ export const GetUserGithub = () : ReactElement => {
             setLoading(false)
             setRenderUser(data)
 
-         if (data) {
+        if (data) {
             setRenderUser({
               name: data.name,
               avatar_url: data.avatar_url,
@@ -80,17 +81,17 @@ export const GetUserGithub = () : ReactElement => {
         }
         
   return (
-    <main className="max-w-[1440px]">
-    <div className="absolute top-5 left-20">
+    <main className="relative max-w-[1440px]">
+    <div className="absolute top-[-70px] left-[-50px]">
       <img src={Camada} alt="Camada Quadrada" className="h-[120px] w-[150px]" />
     </div>
-    <div className="absolute top-0 right-0">
+    <div className="absolute right-[-140px] top-[-90px]">
       <img src={Elipse1} alt="Elipse 1" className="h-100 w-130"/>
     </div>
-    <div className="absolute top-15 left-0">
+    <div className="absolute left-[-150px]">
       <img src={Elipse2} alt="Elipse 2" />
     </div>
-    <div className="bg-black text-white min-w-[1100px] min-h-[450px] flex flex-col items-center relative ">
+    <div className="bg-black text-white min-w-[1100px] min- h-[450px] flex flex-col items-center relative">
         <div className="flex justify-center items-center m-10">
         {/* <span className="ml-[10px] font-black"></span> */}
             <img src={LogoGithub} alt="Github Icon" className="h-[50px] w-50-[px]" />
@@ -99,20 +100,19 @@ export const GetUserGithub = () : ReactElement => {
             <img src={GitName} alt="name" className="h-[38px]"/>
             </div>
         </div>       
-        <div className="flex items-center bg-white text-black border-borderborder-1 rounded-lg h-12 p-4 ml-4">
-        <input className="w-110 h-12 focus:outline-none placeholder-black font-medium"type="search" placeholder="Digite um usuário do Github" onKeyUp={halldeKeyup}/>
-        <span onClick={haddleUser}>&#128269;</span>  
-        {/* <FontAwesomeIcon icon="fa-thin fa-magnifying-glass" /> */}
+        <div className="flex items-center bg-white text-black rounded-lg h-12 py-4 pl-4 ml-4">
+        <input className="w-110 h-12 focus:outline-none placeholder-black font-medium"  type="search" placeholder="Digite um usuário do Github" onKeyUp={halldeKeyup}/>
+        <FontAwesomeIcon icon={faMagnifyingGlass} className="text-white bg-lupa rounded-lg p-[15px] border-border border-1 " onClick={haddleUser}/>
         </div>
 
         {loading && 
-               <p>CARREGANDO...</p>
+              <p>CARREGANDO...</p>
         }
         {renderUser && !loading &&
             <div>
-               <h1>{renderUser.name}</h1>
-               <p>{renderUser.bio? renderUser.bio : 'sem bio'}</p>
-               <img src={renderUser.avatar_url} alt="image" />
+              <h1>{renderUser.name}</h1>
+              <p>{renderUser.bio? renderUser.bio : 'sem bio'}</p>
+              <img src={renderUser.avatar_url} alt="image" />
             </div>
       
         }
